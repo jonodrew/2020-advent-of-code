d043cc32f1bc23c9e17b1f7e1b401908a3fcb48d
Jonathan Kerr <jonathan.kerr@cabinetoffice.gov.uk>
2020-12-02 10:19:19 +0000

    Solve Day Two
    This may be slightly over-engineered. I've done my best to make this as simple and readable as possible. Note that the second class inherits almost everything from the class above - not sure I'm completely happy with it, and whether it should be three classes.

---
+++ two/main.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+class DayTwo(object):
+    def __init__(self):
+        with open('two/input.txt', 'r') as reader:
+            self.inputs = [self.process_input_line(line) for line in reader]
+
+        self.valid_passwords = self.check_validity()
+
+    def process_input_line(self, reader_object_line: str):
+        parts = reader_object_line.split(' ')
+        min_max = parts[0].split('-')
+        min_uses = int(min_max[0])
+        max_uses = int(min_max[1])
+        letter_rule = parts[1][0]
+        password = parts[2].rstrip()
+        return (min_uses, max_uses, letter_rule, password)
+
+    def check_password_against_rule(self, line_input: Tuple[int, int, str, str]):
+        occurences = line_input[3].count(line_input[2])
+        return occurences >= line_input[0] and occurences <= line_input[1]
+
+    def check_validity(self):
+        return [self.check_password_against_rule(line_input) for line_input in self.inputs].count(True)
+
+
+class DayTwoPartTwo(DayTwo):
+    def __init__(self):
+        super().__init__()
+
+    def check_password_against_rule(self, line_input: Tuple[int, int, str, str]):
+        location_one = line_input[0] - 1
+        location_two = line_input[1] - 1
+        values_at_locations = (line_input[3][location_one], line_input[3][location_two])
+        return values_at_locations.count(line_input[2]) == 1

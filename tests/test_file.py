f951a1901a39d7efc05460e3629a560e331ad8f4
Jonathan Kerr <jonathan.kerr@cabinetoffice.gov.uk>
2020-12-03 10:05:51 +0000

    Added new failing test

--- tests/test_file.py
+++ tests/test_file.py
@@ -3,7 +3,9 @@ import pytest
 
 
 class TestDayThree:
-    @pytest.mark.parametrize("start_position, end_position", [((0, 0), (1, 3))])
+    @pytest.mark.parametrize(
+        "start_position, end_position", [((0, 0), (1, 3)), ((0, 30), (1, 2))]
+    )
     def test_move(self, start_position, end_position):
         d = DayThree("tests/test-three-one.txt")
         assert end_position == d.move(start_position)

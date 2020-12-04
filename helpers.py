2535aa1c88749f554cb53e2922263e72d8495b1f
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-12-04 16:37:28 +0000

    Added a little typing to help elsewhere

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
+from typing import List
+
+
 class ReadLines(object):
     def __init__(self, file_input: str):
         with open(file_input) as reader:
-            self.inputs = [line.rstrip() for line in reader]
+            self.inputs: List[str] = [line.rstrip() for line in reader]

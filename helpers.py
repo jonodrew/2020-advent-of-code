4d9572fa53860e5345c4cd5bb77953f712e0f101
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-12-08 17:30:10 +0000

    Refactored helpers.ReadLines

--- helpers.py
+++ helpers.py
@@ -3,17 +3,14 @@ import itertools
 
 
 class ReadLines(object):
-    def __init__(self, file_input: str):
+    def __init__(self, file_input: Union[str, None] = None, day: str = ""):
+        if file_input is None:
+            file_input = f"/home/jonathan/projects/2020-advent-of-code/{day}/input.txt"
         with open(file_input) as reader:
             self.inputs: List[str] = [line.rstrip() for line in reader]
 
 
 class AdventOfCodeHelpers(ReadLines):
-    def __init__(self, file_input: Union[str, None] = None, day: str = ""):
-        if file_input is None:
-            file_input = f"/home/jonathan/projects/2020-advent-of-code/{day}/input.txt"
-        super(AdventOfCodeHelpers, self).__init__(file_input)
-
     def _group(self) -> List[List[str]]:
         return [
             list(group)

01a45a0345184762a153aa37b62b752f1a66d83f
Jonathan Kerr <jonathandrewkerr@gmail.com>
2020-12-10 16:33:19 +0000

    Wrote small script to autogenerate some necessary files and things

---
+++ new_aoc.py
@@ -0,0 +1,16 @@
+import os
+
+
+class NewAdventOfCode:
+    def __init__(self, day: str):
+        self.day = day
+
+    def new_files_and_folders(self):
+        base_path = "/home/jonathan/projects/2020-advent-of-code/"
+        code_path = os.path.join(base_path, self.day)
+        os.mkdir(code_path)
+        os.mknod(f"{code_path}/{self.day}.py")
+        os.mknod(f"{code_path}/input.txt")
+        test_path = os.path.join(base_path, f"tests/{self.day}")
+        os.mkdir(test_path)
+        os.mknod(f"{test_path}/test_day_{self.day}.py")

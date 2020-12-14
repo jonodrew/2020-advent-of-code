4f813b07b701370f5ef23166e38cae022f8ba40e
Jonathan Kerr <jonathan.kerr@cabinetoffice.gov.uk>
2020-12-14 11:15:28 +0000

    Rewrote so I can use code elsewhere

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ import itertools
 class ReadLines(object):
     def __init__(self, day: str, file_input: Union[str, None] = None):
         if file_input is None:
-            file_input = f"/home/jonathan/projects/2020-advent-of-code/{day}/input.txt"
+            file_input = f"{day}/input.txt"
         with open(file_input) as reader:
             self.inputs: List[str] = [line.rstrip() for line in reader]
 

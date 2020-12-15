9834a243a744cd7600fe47f5c807f7593c99bd65
Jonathan Kerr <jonathan.kerr@cabinetoffice.gov.uk>
2020-12-15 13:15:07 +0000

    Nice and easy
    Today's challenge was really enjoyable. Nothing particularly noteable within.

--- fifteen/fifteen.py
+++ fifteen/fifteen.py
@@ -1,6 +1,27 @@
+from typing import List, Dict, Tuple
+
+
 class Recitation:
-    def __init__(self, puzzle_input=[14, 3, 1, 0, 9, 5]):
+    def __init__(self, puzzle_input=[14, 3, 1, 0, 9, 5], count_to: int = 2020):
+        self.count_to = count_to
         self.inputs = puzzle_input
+        self.current_counter = len(puzzle_input)
+        self.record: Dict[int, List[int]] = {
+            value: [index] for index, value in enumerate(puzzle_input)
+        }
 
     def execute(self) -> int:
-        return 0
+        last_value = self.inputs[-1]
+        while self.current_counter != self.count_to:
+            last_value = self.evaluate(last_value)
+            self.current_counter += 1
+        return last_value
+
+    def evaluate(self, last_value: int) -> int:
+        if last_value not in self.record.keys():
+            self.record[last_value] = [self.current_counter - 1]
+            return 0
+        else:
+            self.record[last_value].append(self.current_counter - 1)
+            record: List[int] = self.record[last_value]
+            return record[-1] - record.pop(-2)

4383220b17e08e50f303be6012f84bfb44525312
Jonathan Kerr <jonathandrewkerr@gmail.com>
2021-05-24 17:28:57 +0100

    Make helpers better

--- helpers.py
+++ helpers.py
@@ -2,12 +2,13 @@ import operator
 from functools import reduce
 from typing import List, Union
 import itertools
+import os
 
 
 class ReadLines(object):
     def __init__(self, day: str, file_input: Union[str, None] = None):
         if file_input is None:
-            file_input = f"{day}/input.txt"
+            file_input = os.path.join(os.getcwd(), f"{day}/input.txt")
         with open(file_input) as reader:
             self.inputs: List[str] = [line.rstrip() for line in reader]
 
